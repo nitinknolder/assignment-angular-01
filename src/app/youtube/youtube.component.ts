2fd812268626fbb776833831df9c2014b94457e6
nitinknolder <nitinarora1519@gmail.com>
2018-03-06 16:22:46 +0530

    Angular assignment added Successfully

--- src/app/youtube/youtube.component.ts
+++ src/app/youtube/youtube.component.ts
@@ -22,30 +22,3 @@ export class YoutubeComponent implements OnInit {
     this.videoLink = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
 }
-//   constructor(private router: ActivatedRoute, private homeService: HomeService) {
-//   }
-//
-//   data = [];
-//   url: string;
-//
-//   ngOnInit() {
-//     this.checkVideo();
-//     console.log(this.router.snapshot.params['id']);
-//   }
-//
-//   checkVideo() {
-//     console.log('started');
-//     this.homeService.getData().subscribe(
-//       (data: any) => {
-//         this.data = data.items;
-//         console.log(this.data);
-//       },
-//       (err) => {
-//         this.data = err;
-//         console.log('error' + err);
-//       },
-//       () => {
-//         console.log('end');
-//       });
-//   }
-// }
